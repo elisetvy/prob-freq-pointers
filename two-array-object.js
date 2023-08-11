d7b0d780e320854f93a697683f185014d50a001a
Nathan Berger <nberger101@gmail.com>
2023-08-11 19:24:26 -0400

    completed two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,30 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+/**
+ * input:  two arrays, keys and values
+ * output: object of both combined
+ *
+ * ['x', 'y', 'z'], [1, 2]);
+  // {'x': 1, 'y': 2, 'z': null}
+
+  ['a', 'b', 'c'], [1, 2, 3, 4]);
+  // {'a': 1, 'b': 2, 'c': 3}
+
+  loop over keys array
+  add all keys and corresponding values
+  if no values present, value of key (at corresponding index) is set to null
+ */
+
+
+
+function twoArrayObject(keys, values) {
+  const combinedObject = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    if (!values[i]) {
+      combinedObject[keys[i]] = null;
+    }
+    else {
+      combinedObject[keys[i]] = values[i];
+    }
+  }
+  return combinedObject;
 }
